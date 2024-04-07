654928f5e0c68db4389f345d14687bb3a52d24a7
Matamir <mhirsch217@gmail.com>
2024-04-07 15:19:28 -0400

    added pitch throwing in baseball

--- src/components/Baseball.js
+++ src/components/Baseball.js
@@ -3,7 +3,7 @@ import { useEffect } from "react";
 const Baseball = () => {
 
 
-    
+
 
     useEffect(() => {
 
@@ -15,24 +15,24 @@ const Baseball = () => {
             canvas.width = document.documentElement.clientWidth;
             canvas.height = document.documentElement.clientHeight;
 
-////////////// Green //////////////
-            context.fillStyle = "rgb(0, 100, 0)"        
-            context.fillRect(0,0,canvas.width, canvas.height)
+            ////////////// Green //////////////
+            context.fillStyle = "rgb(0, 100, 0)"
+            context.fillRect(0, 0, canvas.width, canvas.height)
 
-////////////// Bases //////////////    
-            const xHome = canvas.width*2/4 + canvas.height*0/4
-            const yHome = canvas.height*3/4
+            ////////////// Bases //////////////    
+            const xHome = canvas.width * 2 / 4 + canvas.height * 0 / 4
+            const yHome = canvas.height * 3 / 4
 
-            const xFirst = canvas.width*2/4 + canvas.height*1/4
-            const yFirst = canvas.height*2/4
-            
-            const xSecond = canvas.width*2/4 + canvas.height*0/4
-            const ySecond = canvas.height*1/4
-            
-            const xThird = canvas.width*2/4 + canvas.height*-1/4
-            const yThird = canvas.height*2/4
+            const xFirst = canvas.width * 2 / 4 + canvas.height * 1 / 4
+            const yFirst = canvas.height * 2 / 4
 
-////////////// Diamond //////////////    
+            const xSecond = canvas.width * 2 / 4 + canvas.height * 0 / 4
+            const ySecond = canvas.height * 1 / 4
+
+            const xThird = canvas.width * 2 / 4 + canvas.height * -1 / 4
+            const yThird = canvas.height * 2 / 4
+
+            ////////////// Diamond //////////////    
             context.lineWidth = 1;
             context.fillStyle = "rgb(75, 50, 0)";
             context.beginPath();
@@ -43,7 +43,7 @@ const Baseball = () => {
             context.lineTo(xHome, yHome);
             context.fill();
 
-////////////// Foul Lines //////////////
+            ////////////// Foul Lines //////////////
             context.lineWidth = 5;
             context.strokeStyle = "rgb(255,255,255)";
             context.beginPath();
@@ -51,8 +51,8 @@ const Baseball = () => {
             // Third Base Foul Line
             let xEndLeftFoulLine = 0;
             let yEndLeftFoulLine = yThird - ((xThird - xEndLeftFoulLine) * (yThird - yHome)) / (xThird - xHome);
-            
-            context.moveTo(xHome,yHome);
+
+            context.moveTo(xHome, yHome);
             context.lineTo(xEndLeftFoulLine, yEndLeftFoulLine);
 
 
@@ -60,55 +60,55 @@ const Baseball = () => {
             let xEndRightFoulLine = canvas.width;
             let yEndRightFoulLine = yFirst - ((xFirst - xEndRightFoulLine) * (yFirst - yHome)) / (xFirst - xHome);
 
-            context.moveTo(xHome,yHome);
+            context.moveTo(xHome, yHome);
             context.lineTo(xEndRightFoulLine, yEndRightFoulLine);
             context.stroke();
 
 
-////////////// Bases //////////////
-            const BASE_SIZE = 25 
+            ////////////// Bases //////////////
+            const BASE_SIZE = canvas.height / 50
             context.fillStyle = "rgb(255,255,255)";
 
             // Home Plate
-            context.moveTo(xHome,                   yHome)
-            context.lineTo(xHome - BASE_SIZE,       yHome - BASE_SIZE)
-            context.lineTo(xHome,                   yHome - BASE_SIZE * 2)
-            context.lineTo(xHome + BASE_SIZE,       yHome - BASE_SIZE)
+            context.moveTo(xHome, yHome)
+            context.lineTo(xHome - BASE_SIZE, yHome - BASE_SIZE)
+            context.lineTo(xHome, yHome - BASE_SIZE * 2)
+            context.lineTo(xHome + BASE_SIZE, yHome - BASE_SIZE)
             context.fill()
 
 
             // First Base
-            context.moveTo(xFirst,                  yFirst)
-            context.lineTo(xFirst - BASE_SIZE,      yFirst - BASE_SIZE)
-            context.lineTo(xFirst - BASE_SIZE * 2,  yFirst)
-            context.lineTo(xFirst - BASE_SIZE,      yFirst + BASE_SIZE)
+            context.moveTo(xFirst, yFirst)
+            context.lineTo(xFirst - BASE_SIZE, yFirst - BASE_SIZE)
+            context.lineTo(xFirst - BASE_SIZE * 2, yFirst)
+            context.lineTo(xFirst - BASE_SIZE, yFirst + BASE_SIZE)
             context.fill()
 
 
             // Second Base
-            context.moveTo(xSecond,                 ySecond)
-            context.lineTo(xSecond + BASE_SIZE,     ySecond + BASE_SIZE)
-            context.lineTo(xSecond,                 ySecond + BASE_SIZE * 2)
-            context.lineTo(xSecond - BASE_SIZE,     ySecond + BASE_SIZE)
+            context.moveTo(xSecond, ySecond)
+            context.lineTo(xSecond + BASE_SIZE, ySecond + BASE_SIZE)
+            context.lineTo(xSecond, ySecond + BASE_SIZE * 2)
+            context.lineTo(xSecond - BASE_SIZE, ySecond + BASE_SIZE)
             context.fill()
 
 
             // Third Base
-            context.moveTo(xThird,                  yThird)
-            context.lineTo(xThird + BASE_SIZE,      yThird - BASE_SIZE)
-            context.lineTo(xThird + BASE_SIZE * 2,  yThird)
-            context.lineTo(xThird + BASE_SIZE,      yThird + BASE_SIZE)
+            context.moveTo(xThird, yThird)
+            context.lineTo(xThird + BASE_SIZE, yThird - BASE_SIZE)
+            context.lineTo(xThird + BASE_SIZE * 2, yThird)
+            context.lineTo(xThird + BASE_SIZE, yThird + BASE_SIZE)
             context.fill()
 
 
             // Mound
             const xMound = xThird + (xFirst - xThird) / 2
-            const yMound = yFirst - 50
-            
+            const yMound = yFirst - BASE_SIZE * 2
+
             context.lineWidth = 10;
             context.beginPath()
-            context.moveTo(xMound - 15,         yMound)
-            context.lineTo(xMound + 15,         yMound)
+            context.moveTo(xMound - BASE_SIZE / 2, yMound)
+            context.lineTo(xMound + BASE_SIZE / 2, yMound)
             context.stroke();
             context.fill();
 
@@ -121,32 +121,98 @@ const Baseball = () => {
         }
 
 
+        let pitch = { x: canvas.width / 2, y: canvas.height / 2, xVel: 0, yVel: 0, xAccel: 0, yAccel: 0 }
 
-        
+        function removePitch() {
+            pitch = { x: canvas.width / 2, y: canvas.height / 2, xVel: 0, yVel: 0, xAccel: 0, yAccel: 0 }
+        }
+
+        function generatePitch() {
+            canvas.width = document.documentElement.clientWidth;
+            canvas.height = document.documentElement.clientHeight;
+
+            const xHome = canvas.width * 2 / 4 + canvas.height * 0 / 4
+            const yHome = canvas.height * 3 / 4
+
+            const xFirst = canvas.width * 2 / 4 + canvas.height * 1 / 4
+            const yFirst = canvas.height * 2 / 4
+
+            const xSecond = canvas.width * 2 / 4 + canvas.height * 0 / 4
+            const ySecond = canvas.height * 1 / 4
+
+            const BASE_SIZE = canvas.height / 50
+
+            const xThird = canvas.width * 2 / 4 + canvas.height * -1 / 4
+            const yThird = canvas.height * 2 / 4
+
+            const xMound = xThird + (xFirst - xThird) / 2
+            const yMound = yFirst - BASE_SIZE * 2
+
+            let x = xMound;
+            let y = yMound;
+            let xVel = 0;
+            let yVel = 0;
+            let xAccel = Math.random() * .04 - .02;
+            let yAccel = Math.random() * .15 + .05;
+
+            pitch = { x, y, xVel, yVel, xAccel, yAccel };
+        }
+
+        function drawPitch() {
+            context.clearRect(0, 0, canvas.height, canvas.width);
+            drawField();
+
+            let { x, y, xVel, yVel, xAccel, yAccel } = pitch
+
+            x += xVel;
+            y += yVel;
+
+            xVel += xAccel;
+            yVel += yAccel;
+
+            context.fillStyle = "rgb(255,255,255)"
+            context.beginPath();
+            context.arc(x, y, 5, 0, 360);
+            context.fill();
+
+            pitch = { x, y, xVel, yVel, xAccel, yAccel };
+
+            requestAnimationFrame(drawPitch)
+        }
+
+
         // Draw everything
         drawField();
+        drawPitch();
+
+        // Throw a pitch when click
+        window.addEventListener("click", onmousedown);
+        onmousedown = (event) => {
+            generatePitch();
+        }
 
         // Automatically resize everything if the window size is changed
-        const handleResize = () => {drawField();}
+        const handleResize = () => { drawField(); }
         window.addEventListener("resize", handleResize);
-        return() => {
-            window.removeEventListener("resize",handleResize);
-            
+
+        return () => {
+            window.removeEventListener("resize", handleResize);
+            window.removeEventListener("click", onmousedown);
         }
     }, []);
 
     return (
         <div>
-            <canvas 
-                style = {{
+            <canvas
+                style={{
                     backgroundColor: "",
-                    display:"block",
-                }} 
+                    display: "block",
+                }}
                 id="canvas">
-                
+
             </canvas>
 
-            
+
         </div>
     )
 }
--- src/components/Bouncy.js
+++ src/components/Bouncy.js
@@ -3,7 +3,7 @@ import { useEffect } from "react";
 const Bouncy = () => {
 
 
-    
+
 
     useEffect(() => {
 
@@ -15,37 +15,37 @@ const Bouncy = () => {
             canvas.width = document.documentElement.clientWidth;
             canvas.height = document.documentElement.clientHeight;
 
-////////////// Background //////////////
-            context.fillStyle = "rgb(91, 206, 250)"        
-            context.fillRect(0,0,canvas.width, canvas.height)
+            ////////////// Background //////////////
+            context.fillStyle = "rgb(91, 206, 250)"
+            context.fillRect(0, 0, canvas.width, canvas.height)
 
-////////////// Grid //////////////    
+            ////////////// Grid //////////////    
             context.lineWidth = 1;
             context.strokeStyle = "rgb(0,0,75)";
             for (var x = 0; x < canvas.width; x += 50) {
                 for (var y = 0; y < canvas.height; y += 50) {
-                context.strokeRect(x, y , 50, 50);
+                    context.strokeRect(x, y, 50, 50);
                 }
             }
 
-////////////// Bwol //////////////
+            ////////////// Bwol //////////////
             context.strokeStyle = "rgb(245, 169, 184)";
             context.lineWidth = 10;
             context.beginPath();
-            context.arc( canvas.width/2, canvas.height/2, 500, 0, Math.PI)
+            context.arc(canvas.width / 2, canvas.height / 2, 500, 0, Math.PI)
             //context.fill();
             context.stroke();
 
         }
 
-        
+
 
 
         // Function to get a random color
         function RandomColor() {
-            let r = Math.floor(Math.random()*255);
-            let g = Math.floor(Math.random()*255);
-            let b = Math.floor(Math.random()*255);
+            let r = Math.floor(Math.random() * 255);
+            let g = Math.floor(Math.random() * 255);
+            let b = Math.floor(Math.random() * 255);
             return "rgb(" + r + "," + g + "," + b + ")"
         }
 
@@ -56,13 +56,13 @@ const Bouncy = () => {
 
         // Adds a bouncy thing at a random location with random velocity
         function addBouncyThing() {
-            let x = Math.random()*(canvas.width - 50);
-            let y = Math.random()*(canvas.height - 50);
-            let velX = Math.random() * 5 ;
-            let velY = Math.random() * 5 ;
+            let x = Math.random() * (canvas.width - 50);
+            let y = Math.random() * (canvas.height - 50);
+            let velX = Math.random() * 5;
+            let velY = Math.random() * 5;
             let color = RandomColor();
 
-            bouncingObject.push({x, y, velX, velY, color})
+            bouncingObject.push({ x, y, velX, velY, color })
         }
 
         // Adds a bouncy thing at mouse click location with no velocity
@@ -75,42 +75,42 @@ const Bouncy = () => {
             let accelX = .01
             let accelY = .081
 
-            bouncingObject.push({x, y, velX, velY, accelX, accelY, color})
+            bouncingObject.push({ x, y, velX, velY, accelX, accelY, color })
         }
 
         function drawBouncyThings() {
-            context.clearRect( 0, 0, canvas.height, canvas.width );
+            context.clearRect(0, 0, canvas.height, canvas.width);
             drawBoard();
 
             bouncingObject.forEach((object) => {
 
-                let {x, y, velX, velY, accelX, accelY, color} = object;
+                let { x, y, velX, velY, accelX, accelY, color } = object;
 
                 x += velX;
                 y += velY;
 
                 velY += accelY
                 velX += accelX
-                
 
-                if(x > canvas.width-25 || x < 25) {
+
+                if (x > canvas.width - 25 || x < 25) {
                     velX = -velX
                     velX = velX * .9
                     //color = RandomColor();
                     x = (x < 25) ? 26 : canvas.width - 25
                 }
-    
-                if(y > canvas.height-25 ) {
+
+                if (y > canvas.height - 25) {
                     velY = -velY
                     velY = velY * .975
                     //color = RandomColor();
                     y = (y < 25) ? 26 : canvas.height - 25
                 }
-    
-                
+
+
                 context.fillStyle = color;
                 context.beginPath();
-                context.arc( x, y, 25, 0, 360)
+                context.arc(x, y, 25, 0, 360)
                 context.fill();
 
                 object.x = x;
@@ -119,19 +119,19 @@ const Bouncy = () => {
                 object.velY = velY;
                 object.accelX = accelX;
                 object.accelY = accelY;
-                
+
                 object.color = color;
-                
+
             })
 
-            
+
             requestAnimationFrame(drawBouncyThings);
-            
+
         }
 
         // Add a bunch of balls
         let NUMBER_STARTING_BALLS = 0
-        for ( let i = 0; i < NUMBER_STARTING_BALLS; i++) {
+        for (let i = 0; i < NUMBER_STARTING_BALLS; i++) {
             addBouncyThing();
         }
 
@@ -147,27 +147,27 @@ const Bouncy = () => {
         }
 
         // Automatically resize everything if the window size is changed
-        const handleResize = () => {drawBoard();}
+        const handleResize = () => { drawBoard(); }
         window.addEventListener("resize", handleResize);
-        return() => {
-            window.removeEventListener("resize",handleResize);
-            window.removeEventListener("click",onclick);
-            
+        return () => {
+            window.removeEventListener("resize", handleResize);
+            window.removeEventListener("click", onclick);
+
         }
     }, []);
 
     return (
         <div>
-            <canvas 
-                style = {{
+            <canvas
+                style={{
                     backgroundColor: "",
-                    display:"block",
-                }} 
+                    display: "block",
+                }}
                 id="canvas">
-                
+
             </canvas>
 
-            
+
         </div>
     )
 }
