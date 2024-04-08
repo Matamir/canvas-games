1a492fc233e16187c1c63046ce4f5db3b45a673d
Matamir <mhirsch217@gmail.com>
2024-04-08 13:59:45 -0400

    swinging bat

--- src/components/Baseball.js
+++ src/components/Baseball.js
@@ -2,35 +2,63 @@ import { useEffect } from "react";
 
 const Baseball = () => {
 
-
-
-
     useEffect(() => {
 
         var canvas = document.getElementById("canvas");
         var context = canvas.getContext("2d");
 
-        function drawField() {
+        canvas.width = document.documentElement.clientWidth;
+        canvas.height = document.documentElement.clientHeight;
+
+        var xHome = canvas.width * 2 / 4 + canvas.height * 0 / 4
+        var yHome = canvas.height * 3 / 4
+
+        var xFirst = canvas.width * 2 / 4 + canvas.height * 1 / 4
+        var yFirst = canvas.height * 2 / 4
+
+        var xSecond = canvas.width * 2 / 4 + canvas.height * 0 / 4
+        var ySecond = canvas.height * 1 / 4
+
+        var BASE_SIZE = canvas.height / 50
+
+        var xThird = canvas.width * 2 / 4 + canvas.height * -1 / 4
+        var yThird = canvas.height * 2 / 4
+
+        var xMound = xThird + (xFirst - xThird) / 2
+        var yMound = yFirst - BASE_SIZE * 2
+
+
+        function updatefield() {
+            canvas = document.getElementById("canvas");
+            context = canvas.getContext("2d");
 
             canvas.width = document.documentElement.clientWidth;
             canvas.height = document.documentElement.clientHeight;
 
+            xHome = canvas.width * 2 / 4 + canvas.height * 0 / 4
+            yHome = canvas.height * 3 / 4
+
+            xFirst = canvas.width * 2 / 4 + canvas.height * 1 / 4
+            yFirst = canvas.height * 2 / 4
+
+            xSecond = canvas.width * 2 / 4 + canvas.height * 0 / 4
+            ySecond = canvas.height * 1 / 4
+
+            BASE_SIZE = canvas.height / 50
+
+            xThird = canvas.width * 2 / 4 + canvas.height * -1 / 4
+            yThird = canvas.height * 2 / 4
+
+            xMound = xThird + (xFirst - xThird) / 2
+            yMound = yFirst - BASE_SIZE * 2
+        }
+
+
+        function drawField() {
+
             ////////////// Green //////////////
             context.fillStyle = "rgb(0, 100, 0)"
             context.fillRect(0, 0, canvas.width, canvas.height)
-
-            ////////////// Bases //////////////    
-            const xHome = canvas.width * 2 / 4 + canvas.height * 0 / 4
-            const yHome = canvas.height * 3 / 4
-
-            const xFirst = canvas.width * 2 / 4 + canvas.height * 1 / 4
-            const yFirst = canvas.height * 2 / 4
-
-            const xSecond = canvas.width * 2 / 4 + canvas.height * 0 / 4
-            const ySecond = canvas.height * 1 / 4
-
-            const xThird = canvas.width * 2 / 4 + canvas.height * -1 / 4
-            const yThird = canvas.height * 2 / 4
 
             ////////////// Diamond //////////////    
             context.lineWidth = 1;
@@ -66,7 +94,6 @@ const Baseball = () => {
 
 
             ////////////// Bases //////////////
-            const BASE_SIZE = canvas.height / 50
             context.fillStyle = "rgb(255,255,255)";
 
             // Home Plate
@@ -102,9 +129,6 @@ const Baseball = () => {
 
 
             // Mound
-            const xMound = xThird + (xFirst - xThird) / 2
-            const yMound = yFirst - BASE_SIZE * 2
-
             context.lineWidth = 10;
             context.beginPath()
             context.moveTo(xMound - BASE_SIZE / 2, yMound)
@@ -123,44 +147,22 @@ const Baseball = () => {
 
         let pitch = { x: canvas.width / 2, y: canvas.height / 2, xVel: 0, yVel: 0, xAccel: 0, yAccel: 0 }
 
-        function removePitch() {
-            pitch = { x: canvas.width / 2, y: canvas.height / 2, xVel: 0, yVel: 0, xAccel: 0, yAccel: 0 }
-        }
-
         function generatePitch() {
-            canvas.width = document.documentElement.clientWidth;
-            canvas.height = document.documentElement.clientHeight;
-
-            const xHome = canvas.width * 2 / 4 + canvas.height * 0 / 4
-            const yHome = canvas.height * 3 / 4
-
-            const xFirst = canvas.width * 2 / 4 + canvas.height * 1 / 4
-            const yFirst = canvas.height * 2 / 4
-
-            const xSecond = canvas.width * 2 / 4 + canvas.height * 0 / 4
-            const ySecond = canvas.height * 1 / 4
-
-            const BASE_SIZE = canvas.height / 50
-
-            const xThird = canvas.width * 2 / 4 + canvas.height * -1 / 4
-            const yThird = canvas.height * 2 / 4
-
-            const xMound = xThird + (xFirst - xThird) / 2
-            const yMound = yFirst - BASE_SIZE * 2
 
             let x = xMound;
             let y = yMound;
             let xVel = 0;
             let yVel = 0;
-            let xAccel = Math.random() * .04 - .02;
-            let yAccel = Math.random() * .15 + .05;
+            let xAccel = Math.random() * .004 - .002;
+            let yAccel = Math.random() * .015 + .005;
 
             pitch = { x, y, xVel, yVel, xAccel, yAccel };
         }
 
         function drawPitch() {
-            context.clearRect(0, 0, canvas.height, canvas.width);
+            context.clearRect(0, 0, canvas.width, canvas.height);
             drawField();
+            drawBat();
 
             let { x, y, xVel, yVel, xAccel, yAccel } = pitch
 
@@ -171,19 +173,68 @@ const Baseball = () => {
             yVel += yAccel;
 
             context.fillStyle = "rgb(255,255,255)"
+            context.strokeStyle = "rgb(0,0,0)"
+            context.lineWidth = 1;
             context.beginPath();
             context.arc(x, y, 5, 0, 360);
+            context.stroke();
             context.fill();
 
             pitch = { x, y, xVel, yVel, xAccel, yAccel };
 
-            requestAnimationFrame(drawPitch)
         }
 
 
-        // Draw everything
-        drawField();
-        drawPitch();
+
+        let bat = {
+            angle: Math.PI / 2,
+            xBatStart: xHome - BASE_SIZE,
+            yBatStart: yHome - BASE_SIZE,
+        }
+
+        function resetBat() {
+            bat = {
+                angle: Math.PI / 2,
+                xBatStart: xHome - BASE_SIZE,
+                yBatStart: yHome - BASE_SIZE,
+            }
+        }
+
+        function drawBat() {
+            context.strokeStyle = "rgb(0,0,0)"
+            let xBatEnd = bat.xBatStart + Math.cos(bat.angle) * BASE_SIZE * 2;
+            let yBatEnd = bat.yBatStart + Math.sin(bat.angle) * BASE_SIZE * 2;
+            
+            context.lineWidth = 5;
+            context.beginPath();
+            context.moveTo(bat.xBatStart, bat.yBatStart);
+            context.lineTo(xBatEnd, yBatEnd);
+            context.stroke();
+
+        }
+
+        function swingBat() {
+
+            context.clearRect(0, 0, canvas.width, canvas.height);
+
+            let angle = bat.angle - 0.0174533*10;
+
+            bat = {
+                xBatStart: xHome - BASE_SIZE,
+                yBatStart: yHome - BASE_SIZE,
+                angle: angle,
+            }
+
+        }
+
+        function animate() {
+            drawField();
+            drawBat();
+            drawPitch();
+            requestAnimationFrame(animate);
+        }
+
+        animate();
 
         // Throw a pitch when click
         window.addEventListener("click", onmousedown);
@@ -191,8 +242,24 @@ const Baseball = () => {
             generatePitch();
         }
 
+        // Swing bat when spacebar
+        window.addEventListener("spacedown", onkeydown);
+        onkeydown = (event) => {
+            if(event.code == "Space") {
+                swingBat();
+            }
+        }
+
+        // Swing bat when spacebar
+        window.addEventListener("spacedown", onkeyup);
+        onkeyup = (event) => {
+            if(event.code == "Space") {
+                resetBat();
+            }
+        }
+
         // Automatically resize everything if the window size is changed
-        const handleResize = () => { drawField(); }
+        const handleResize = () => { updatefield(); drawField(); }
         window.addEventListener("resize", handleResize);
 
         return () => {
