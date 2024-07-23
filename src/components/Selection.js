025f58b4d072f39118b64722b178e2b9866cec5b
Matamir <mhirsch217@gmail.com>
2024-07-23 15:14:05 -0400

    made a visual display for selection options that can scale with any options

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,40 +1,67 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { useState, useEffect } from "react";
+
+import bouncyImage from "../images/Bouncy.png"
+import baseballImage from "../images/Baseball.png"
+import circleImage from "../images/Circle.png"
+import estimationGameImage from "../images/EstimationGame.png"
+import colorGuessrImage from "../images/ColorGuessr.png"
+import colorMatcherImage from "../images/ColorMatcher.png"
+
 
 const Selection = () => {
 
     let opts =
-        [{ name: "Bouncy", link: "./Bouncy" },
-        { name: "Baseball", link: "./Baseball" },
-        { name: "Circle", link: "./Circle" },
-        { name: "Estimation Game", link: "./EstimationGame" },
-        { name: "Color Guessr", link: "./ColorGuessr" },
-        { name: "Color Matcher", link: "./ColorMatcher" },
+        [{ name: "Bouncy", link: "./Bouncy", image: bouncyImage },
+        { name: "Baseball", link: "./Baseball", image: baseballImage },
+        { name: "Circle", link: "./Circle", image: circleImage },
+        { name: "Estimation Game", link: "./EstimationGame", image: estimationGameImage },
+        { name: "Color Guessr", link: "./ColorGuessr", image: colorGuessrImage },
+        { name: "Color Matcher", link: "./ColorMatcher", image: colorMatcherImage },
         ]
 
-    const optionsDisplay = () => {
+    const [size, setSize] = useState("vw")
 
-        return (
-            <div>
-                {opts.map((option) => {
-                    return (
-                        <div>
-                            <NavLink to={option.link}>
-                                {option.name}
-                            </NavLink>
-                            <br />
-                        </div>
-                    )
-                })}
-            </div>
-        );
-
+    function updateWindowSize() {
+        if (window.innerWidth < window.innerHeight) {
+            setSize("vh")
+        } else {
+            setSize("vw")
+        }
     }
+
+    useEffect(() => { updateWindowSize(); }, []);
+
+    window.addEventListener("resize", updateWindowSize);
 
     return (
         <nav>
-            {optionsDisplay()}
+
+            <div className="row">
+                {opts.map((option) => {
+
+                    return (
+                        <NavLink
+                            className="col-1 border"
+                            style={{
+                                height: "33" + size,
+                                paddingLeft: "10vw",
+                                width: "33" + size,
+                                backgroundImage: `url(${option.image})`,
+                                backgroundSize: "cover",
+                                backgroundPosition: "center"
+                            }}
+                            to={option.link}>
+                        </NavLink>
+                    )
+
+                })
+                }
+            </div >
         </nav>
-    )
+    );
+
+
 }
 export default Selection;
