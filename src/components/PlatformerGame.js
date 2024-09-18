66a65993005d17c3de46b152b8018a7d49e2dc1b
Matamir <mhirsch217@gmail.com>
2024-09-17 23:41:41 -0400

    platformGame set up with game area and player, key pressing set up with keyUp and keyDown

--- src/components/PlatformerGame.js
+++ src/components/PlatformerGame.js
@@ -9,40 +9,117 @@ const PlatformerGame = () => {
         var canvas = document.getElementById("canvas");
         var context = canvas.getContext("2d");
 
+        let SCREEN_SIZE = 1000
+        let PLAYER_SIZE = 10
+        let PLAYER_SPEED = 0.05
+
+        let xOrigin = canvas.width / 2 - SCREEN_SIZE / 4
+        let yOrigin = canvas.height / 2 - SCREEN_SIZE / 4
+        let xGameSize = SCREEN_SIZE / 2
+        let yGameSize = SCREEN_SIZE / 2
+
+        let pressedKeys = new Set()
 
         function updateCanvasSize() {
             canvas.width = document.documentElement.clientWidth;
             canvas.height = document.documentElement.clientHeight;
+            xOrigin = canvas.width / 2 - SCREEN_SIZE / 4
+            yOrigin = canvas.height / 2 - SCREEN_SIZE / 4
+            xGameSize = SCREEN_SIZE / 2
+            yGameSize = SCREEN_SIZE / 2
         }
 
+        // const handleResize = () => { updateCanvasSize(); }
+        // window.addEventListener("resize", handleResize);
 
-        
+        function movePlayer() {
+            if (player.x + player.xVel > xGameSize - PLAYER_SIZE) {
+                player.x = xGameSize - PLAYER_SIZE
+                player.xVel = 0
+            } else if (player.x + player.xVel < 0) {
+                player.x = 0
+                player.xVel = 0
+            }
 
-        const handleResize = () => { updateCanvasSize(); }
-        window.addEventListener("resize", handleResize);
-        
 
+            if (player.y + player.yVel > yGameSize - PLAYER_SIZE) {
+                player.y = yGameSize - PLAYER_SIZE
+                player.yVel = 0
+            } else if (player.y + player.yVel < 0) {
+                player.y = 0
+                player.yVel = 0
+            }
+
+
+            player.x += player.xVel
+            player.y += player.yVel
+        }
 
         function animate() {
+            updateCanvasSize()
+            movePlayer()
+            controls()
 
+            context.fillStyle = "white"
+            context.fillRect(xOrigin, yOrigin, xGameSize, yGameSize)
+
+            context.fillStyle = "red"
+            context.fillRect(xOrigin + player.x, yOrigin + player.y, PLAYER_SIZE, PLAYER_SIZE)
+
+            requestAnimationFrame(animate)
         }
 
-        function jump() {
+        const player = {}
+        player.x = 0;
+        player.y = 0;
+        player.xVel = 0;
+        player.yVel = 0;
 
+        function jump() {
+            console.log("JUMPING")
         }
 
         animate();
 
-        // Swing bat when spacebar
-        window.addEventListener("spacedown", onkeydown);
-        onkeydown = (event) => {
-            if (event.code == "Space") {
+        // Function to handle keyboard presses
+        function controls(pollingRate) {
+
+            if (pressedKeys.has("Space")) {
                 jump();
             }
+
+            if (pressedKeys.has("KeyW")) {
+                player.yVel -= PLAYER_SPEED
+            }
+
+            if (pressedKeys.has("KeyS")) {
+                player.yVel += PLAYER_SPEED
+            }
+
+            if (pressedKeys.has("KeyA")) {
+                player.xVel -= PLAYER_SPEED
+            }
+
+            if (pressedKeys.has("KeyD")) {
+                player.xVel += PLAYER_SPEED
+            }
+
+        }
+
+        // Swing bat when spacebar
+        window.addEventListener("keyDown", onkeydown);
+        onkeydown = (event) => {
+            pressedKeys.add(event.code)
+        }
+
+        window.addEventListener("keyUp", onkeyup);
+        onkeyup = (event) => {
+            pressedKeys.delete(event.code)
         }
 
         return () => {
-            window.removeEventListener("spacedown", onkeydown);
+            window.removeEventListener("keyDown", onkeydown);
+            window.removeEventListener("keyUp", onkeyup);
         }
     }, []);
 
@@ -50,7 +127,7 @@ const PlatformerGame = () => {
         <div>
             <canvas
                 style={{
-                    backgroundColor: "lightblue",
+                    backgroundColor: "rgb(20,0,50)",
                     display: "block",
                 }}
                 id="canvas">
