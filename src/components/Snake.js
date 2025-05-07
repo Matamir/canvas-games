6c25b4d521582881579e368cb17fd62087afe8d7
Matamir <mhirsch217@gmail.com>
2025-05-06 22:50:06 -0400

    snake is drawing, tongue messed up on animation

--- src/components/Snake.js
+++ src/components/Snake.js
@@ -7,30 +7,87 @@ const Snake = () => {
         var canvas = document.getElementById("canvas");
         var context = canvas.getContext("2d");
 
-        let snake = [0, 0]
+        let snake = [[0, 0]]
+
 
         function drawGrid() {
-
             canvas.width = document.documentElement.clientWidth;
             canvas.height = document.documentElement.clientHeight;
 
-            for (let x = 0; x < canvas.width; x += 20) {
-                context.moveTo(x, 0);
-                context.lineTo(x, canvas.height);
+
+            let widthStart = canvas.width * .1
+            let heightStart = canvas.height * .1
+            let size = (canvas.width < canvas.height) ? canvas.width * .08 : canvas.height * .08
+
+            for (let x = widthStart; x <= widthStart + (size * 10.1); x += size) {
+                context.moveTo(x, heightStart);
+                context.lineTo(x, heightStart + (size * 10));
             }
 
-            for (let y = 0; y < canvas.height; y += 20) {
-                context.moveTo(0, y);
-                context.lineTo(canvas.width, y);
+            for (let y = heightStart; y <= heightStart + (size * 10.1); y += size) {
+                context.moveTo(widthStart, y);
+                context.lineTo(widthStart + (size * 10), y);
             }
 
+
+            context.strokeStyle = "black";
             context.stroke();
+
         }
 
         drawGrid();
 
+        function drawSnake() {
+
+            let widthStart = canvas.width * .1
+            let heightStart = canvas.height * .1
+            let size = (canvas.width < canvas.height) ? canvas.width * .08 : canvas.height * .08
+
+
+            snake.forEach(snakePart => {
+
+                //context.drawImage(___, widthStart + snakePart[0] * size, heightStart + snakePart[1] * size);
+                context.moveTo(widthStart + snakePart[0] * size, heightStart + snakePart[1] * size)
+                context.lineTo(widthStart + snakePart[0] * size, heightStart + snakePart[1] * size + size)
+                context.lineTo(widthStart + snakePart[0] * size + size, heightStart + snakePart[1] * size + size)
+                context.lineTo(widthStart + snakePart[0] * size + size, heightStart + snakePart[1] * size)
+            })
+
+            context.fillStyle = "green";
+            context.fill();
+
+            // DRAW TONGUE
+            context.beginPath();
+            context.moveTo(widthStart + (snake[0][0] + .5 * size), heightStart + (snake[0][1] * size))
+            context.lineTo(widthStart + (snake[0][0] + .5 * size), heightStart + ((snake[0][1] + .5) * size))
+            context.strokeStyle = "pink";
+            context.lineWidth = size / 10;
+            context.stroke();
+        }
+
+        drawSnake();
+
+
+        function updateSnake() {
+            snake.forEach(snakePart => {
+                snakePart[0] += 0
+                snakePart[1] += 1
+            })
+        }
+
+        function animate() {
+            drawSnake();
+            updateSnake();
+            setTimeout(() => {
+                requestAnimationFrame(animate);
+            }, 333);
+        }
+
+        animate();
+
+
         // Automatically resize everything if the window size is changed
-        const handleResize = () => { drawGrid(); }
+        const handleResize = () => { drawGrid(); drawSnake(); }
         window.addEventListener("resize", handleResize);
 
         return () => {
